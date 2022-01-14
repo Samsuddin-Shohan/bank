d20f4e5ee5d2e65df4e1e3a2e121adc5d46e7fbf
Samsuddin-Shohan <shohan.cse.hstu@gmail.com>
2022-01-14 18:01:32 +0600

    js added

--- js/bank.js
+++ js/bank.js
@@ -1,15 +1,29 @@
-const submitButton = document.getElementById('submit-button');
+document.getElementById('deposit-button').addEventListener('click', ()=>{
+   const depositBox = document.getElementById('deposit-box');
+   const currentDepositValueText = depositBox.value;
+   const currentDepositValueAmount = parseFloat(currentDepositValueText);
+    const previoutDepositeValueText = document.getElementById('deposit-value').innerText;    
+    const previoutDepositeValueAmount = parseFloat(previoutDepositeValueText);
+    const totalDepositeAmount = previoutDepositeValueAmount+currentDepositValueAmount;
+    document.getElementById('deposit-value').innerText = totalDepositeAmount;
+    //updating thee balance
+    const previousBalanceText = document.getElementById('balance-value').innerText;   
+    const previousBalanceAmount = parseFloat(previousBalanceText);
+    const totalBalanceAmount = previousBalanceAmount+ currentDepositValueAmount;
+    document.getElementById('balance-value').innerText = totalBalanceAmount;
 
-
-submitButton.addEventListener('click', function(){
-    const emailField = document.getElementById('userEmail');
-    const passwordField = document.getElementById('userPassword') ;
-   
-    const email = emailField.value;
-    const password = passwordField.value;
-    console.log(email,password);
-    if(email === 'abc@gmail.com' && password === 'abc'){
-        window.location.href = 'banking.html';
-        console.log(valid);
-    }
 });
+document.getElementById('withdraw-button').addEventListener('click',()=>{
+    const withdrawBox = document.getElementById('withdraw-box');
+    const currentWithdrawValueText = withdrawBox.value;
+    const currentWithdrawValueAmount = parseFloat(currentWithdrawValueText);
+     const previoutWithdrawValueText = document.getElementById('withdraw-value').innerText;    
+     const previoutWithdrawValueAmount = parseFloat(previoutWithdrawValueText);
+     const totalWithdrawAmount = previoutWithdrawValueAmount+currentWithdrawValueAmount;
+     document.getElementById('withdraw-value').innerText = totalWithdrawAmount;
+      //updating thee balance
+    const previousBalanceText = document.getElementById('balance-value').innerText;   
+    const previousBalanceAmount = parseFloat(previousBalanceText);
+    const totalBalanceAmount = previousBalanceAmount-currentWithdrawValueAmount;
+    document.getElementById('balance-value').innerText = totalBalanceAmount;
+})
